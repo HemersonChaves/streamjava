605ec210a73a0494343f10ca702d1c4f1ed3b94b
Hemerson Chaves <hemersonchaves20@hotmail.com>
2024-05-07 21:57:50 -0300

    primeiro commit

---
+++ src/main/java/com/example/stream_uece/StreamUeceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stream_uece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamUeceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamUeceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stream_uece/StreamUeceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stream_uece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamUeceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
